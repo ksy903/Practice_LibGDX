fe2e40723f0d35289a6c0d4de0f2bfab1508d270
Jae Lee <ksy903@gmail.com>
2015-06-13 02:17:42 -0500

    Initial commit

---
+++ core/src/com/JaeLee/martianrun/MartianRun.java
@@ -0,0 +1,36 @@
+package com.JaeLee.martianrun;
+
+import com.badlogic.gdx.Game;
+
+public class MartianRun extends Game{
+
+	@Override
+	public void create() {
+		setScreen(new MyScreen());
+	}
+
+	@Override
+	public void dispose(){
+		super.dispose();
+	}
+
+	@Override
+	public void pause() {
+		super.pause();
+	}
+
+	@Override
+	public void resume() {
+		super.resume();
+	}
+
+	@Override
+	public void render() {
+		super.render();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+	}
+}
